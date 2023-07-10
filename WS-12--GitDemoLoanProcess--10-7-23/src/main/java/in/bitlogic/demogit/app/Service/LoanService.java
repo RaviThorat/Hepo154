550b68171dd677c3669f7a3ba64d15482ba684b2
RaviThorat <dm.ravithorat@gmail.com>
2023-07-10 16:48:22 +0530

    new pkg repository

--- WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Service/LoanService.java
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Service/LoanService.java
@@ -1,5 +1,11 @@
 package in.bitlogic.demogit.app.Service;
 
+import java.util.List;
+
+import in.bitlogic.demogit.app.model.GitDemo;
+
 public interface LoanService {
+
+	public List<GitDemo> getLoans();
 
 }
--- WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/controller/LoanController.java
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/controller/LoanController.java
@@ -1,8 +1,24 @@
 package in.bitlogic.demogit.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.bitlogic.demogit.app.Service.LoanService;
+import in.bitlogic.demogit.app.model.GitDemo;
 
 @RestController
 public class LoanController{
+	
+	@Autowired
+	LoanService ls;	
+	
+	// Post API => http://localhost:9090/getLoans
+	@GetMapping("/getLoans")
+	public List<GitDemo> getLoans(){
+		return ls.getLoans();
+	}
 	
 }
