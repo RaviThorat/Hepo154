ecb7525bfc905ea89fd1583c6e7ade323c54261b
RaviThorat <dm.ravithorat@gmail.com>
2023-07-10 15:22:31 +0530

    Initial Commit

---
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Ws12GitDemoLoanProcess10723Application.java
@@ -0,0 +1,13 @@
+package in.bitlogic.demogit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws12GitDemoLoanProcess10723Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws12GitDemoLoanProcess10723Application.class, args);
+	}
+
+}
---
+++ WS-12--GitDemoLoanProcess--10-7-23/src/test/java/in/bitlogic/demogit/app/Ws12GitDemoLoanProcess10723ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bitlogic.demogit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws12GitDemoLoanProcess10723ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
