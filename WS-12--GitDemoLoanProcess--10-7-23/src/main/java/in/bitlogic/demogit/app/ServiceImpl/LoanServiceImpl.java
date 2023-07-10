388c72628a68876fce96cede9a3215d5621d34b6
RaviThorat <dm.ravithorat@gmail.com>
2023-07-10 17:01:58 +0530

    add get api

--- WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/ServiceImpl/LoanServiceImpl.java
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/ServiceImpl/LoanServiceImpl.java
@@ -2,24 +2,30 @@ package in.bitlogic.demogit.app.ServiceImpl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import in.bitlogic.demogit.app.Service.LoanService;
+import in.bitlogic.demogit.app.loanrepository.LoanRepository;
 import in.bitlogic.demogit.app.model.GitDemo;
 
 @Service
 public class LoanServiceImpl implements LoanService 
 {
-
+	@Autowired
+	LoanRepository lr;	
+	
+	//Shubham's API
 	@Override
 	public GitDemo saveP(GitDemo p) {
-		
+		return null;
 	}
 
+	//Ravi's API
 	@Override
 	public List<GitDemo> getLoans() {
 		
-		return null;
+		return lr.findAll();
 	}
 
 }
