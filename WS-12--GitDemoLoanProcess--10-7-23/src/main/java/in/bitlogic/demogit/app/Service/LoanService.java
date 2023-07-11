1e64880c9cee3b87bdce4c138e1c44d124b47460
umesh <usgpatil123@gamil.com>
2023-07-11 18:37:01 +0530

    commit

--- WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Service/LoanService.java
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Service/LoanService.java
@@ -6,21 +6,15 @@ import in.bitlogic.demogit.app.model.GitDemo;
 
 public interface LoanService{
 
-<<<<<<< HEAD
 
 	 public void deleteLoan(int gitId);
-=======
+
 	public List<GitDemo> getLoans();
-<<<<<<< HEAD
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
 
 	public void saveP(GitDemo p);
-<<<<<<< HEAD
+
 	public List<GitDemo> getLoans();
-=======
-=======
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
+
 
 	public GitDemo saveP(GitDemo p);
 	
