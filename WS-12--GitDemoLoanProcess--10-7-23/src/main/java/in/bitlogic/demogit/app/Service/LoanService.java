9d32862c9809d7c4da069139c8a1f614c3f0756c
RaviThorat <dm.ravithorat@gmail.com>
2023-07-11 18:11:07 +0530

    loan service and impl updated

--- WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Service/LoanService.java
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/Service/LoanService.java
@@ -7,31 +7,11 @@ import in.bitlogic.demogit.app.model.GitDemo;
 
 public interface LoanService{
 
-<<<<<<< HEAD
-	public List<GitDemo> getLoans();
-=======
-<<<<<<< HEAD
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
+	public GitDemo saveP(GitDemo p);
 
-<<<<<<< HEAD
-	public void saveP(GitDemo p);
-=======
 	 public void deleteLoan(int gitId);
-=======
-	public List<GitDemo> getLoans();
-<<<<<<< HEAD
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
 
-	public void saveP(GitDemo p);
-<<<<<<< HEAD
 	public List<GitDemo> getLoans();
-=======
-=======
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
-
-	
 
 	public GitDemo updatedata(int gitId);
 
--- WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/ServiceImpl/LoanServiceImpl.java
+++ WS-12--GitDemoLoanProcess--10-7-23/src/main/java/in/bitlogic/demogit/app/ServiceImpl/LoanServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 
 import in.bitlogic.demogit.app.Service.LoanService;
@@ -13,23 +11,15 @@ import in.bitlogic.demogit.app.loanrepository.LoanRepository;
 import in.bitlogic.demogit.app.model.GitDemo;
 
 @Service
-<<<<<<< HEAD
 public class LoanServiceImpl implements LoanService {
 
-=======
-public class LoanServiceImpl implements LoanService 
-{
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
 	@Autowired
-	LoanRepository loanrepo;
+	LoanRepository lr;
 
-
-
+	@Override
 	public GitDemo saveP(GitDemo p) {
-		return null;
+		return lr.save(p);
 	}
-
-
 
 	@Override
 	public List<GitDemo> getLoans() {
@@ -38,24 +28,17 @@ public class LoanServiceImpl implements LoanService
 
 	}
 
-<<<<<<< HEAD
-
 	@Override
 	public GitDemo updatedata(int gitId) {
 
 		return lr.save(gitId);
-=======
-	@Override
-	public void deleteLoan(int gitId) {
-		
-		loanrepo.deleteById(gitId);
 	}
 
 	@Override
-	public List<GitDemo> getLoans() {
-		// TODO Auto-generated method stub
-		return null;
->>>>>>> branch 'main' of https://github.com/RaviThorat/Hepo154.git
+	public void deleteLoan(int gitId) {
+		
+		lr.deleteById(gitId);
 	}
+
 
 }
